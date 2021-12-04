0dee9b43f3cc9fe56341f97f45a01f0e9581c1ac
hen04 <nadia_degtiareva@rambler.ru>
2021-12-04 19:03:56 +0300

    theme switcher

---
+++ build/js/main-script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.onload = function(){
+
+	let page = document.querySelector('body');
+	let themeButton = document.querySelector('.theme-button');
+
+	themeButton.onclick = function() {
+		page.classList.toggle('dark-theme');
+	}
+
+};
